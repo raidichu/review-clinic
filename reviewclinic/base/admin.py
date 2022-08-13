05311a32c003e299b6328ad2ffd3ecf0d2e14ba2
khoalq <khoalq@animverse.com>
2022-08-13 21:15:16 +0700

    Revert "tét comit"
    This reverts commit 49dca845bcf836a600c13d626032609e098862ac.

--- reviewclinic/base/admin.py
+++ reviewclinic/base/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Clinic, Comment, Reply
-dấdasdad
+
 admin.site.register(Clinic)
 admin.site.register(Comment)
 admin.site.register(Reply)
